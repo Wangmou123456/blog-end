9a85b70321891665166ac9c1fe0a61a49dd8e8cb
玄学 <1749298515@qq.com>
2021-08-28 20:27:58 +0800

    '解决跨域问题'

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,13 @@
 const Koa = require("koa");
 // 解析json格式
 const bodyParser = require("koa-bodyparser");
+const cors = require("koa-cors");
 
 const userRoutes = require("../router");
 const { errorHandler } = require("./error-handle");
 const app = new Koa();
 
+app.use(cors());
 app.use(bodyParser());
 userRoutes(app);
 app.on("error", errorHandler);
